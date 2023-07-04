a6d24a6dfa26f3de60948ac0683ca825345de484
Florent BEAUCHAMP <florent.beauchamp@vates.fr>
2023-07-04 14:15:06 +0200

    test and fixes

--- @xen-orchestra/fs/src/dedupLocal.js
+++ @xen-orchestra/fs/src/dedupLocal.js
@@ -2,25 +2,27 @@ import execa from 'execa'
 import LocalHandler from './local'
 import fs from 'fs-extra'
 import assert from 'node:assert'
-import { createHash } from 'node:crypto';
+import { createHash } from 'node:crypto'
 
 export default class DedupedLocalHandler extends LocalHandler {
   #dedupFolder = '/xo-block-store'
-  #hashMethod  = 'sha256'
+  #hashMethod = 'sha256'
   #attributeKey = `user.hash.${this.#hashMethod}`
 
-
-  async _sync(){
+  async _sync() {
     await super._sync()
-      await this.mkdir(this.#dedupFolder)
+    await this.mkdir(this.#dedupFolder)
+  }
+
+  #hash(data) {
+    return createHash(this.#hashMethod).update(data).digest('hex')
   }
 
   // even in a deduplicated handler, dedup is opt in
   async _writeFile(file, data, { flags, dedup }) {
     if (dedup === true) {
+      const hash = this.#hash(data)
       // create the file (if not already present) in the store
-      const hash = createHash(this.#hashMethod).update(data).digest('hex')
-      assert.notStrictEqual(hash, undefined, 'Hash must be defined for deduplication to work ')
       const dedupPath = await this.#writeDeduplicationSource(hash, data)
       // hard link to the target place
       // this linked file will have the same extended attributes
@@ -31,39 +33,44 @@ export default class DedupedLocalHandler extends LocalHandler {
     return super._writeFile(file, data, { flags })
   }
 
-  // this is slower than the non dedup method 
   async _unlink(file) {
-    // get hash
-    let hash 
-    try{
+    let hash
+    try {
+      // get hash before deleting the file
       hash = await this.#getExtendedAttribute(file, this.#attributeKey)
-    }catch(err){
-      // attributes unknown ( a non duplicated file)
-      // whatever : fall back to normal delete 
+    } catch (err) {
+      // attributes unknown (a non duplicated file)
+      // whatever : fall back to normal delete
     }
 
-    // delete file
+    // delete file in place
     await super._unlink(file)
 
     if (hash) {
       const dedupPath = this.getFilePath(this.#computeDeduplicationPath(hash))
-      const { nlink } = await fs.stat(dedupPath)
-      // get the number of copy still using these data
-      // delete source if it's alone
-      if (nlink === 1) {
-        const dedupPath = this.getFilePath(this.#computeDeduplicationPath(hash))
-        await fs.unlink(dedupPath)
+      try{
+        const { nlink } = await fs.stat(dedupPath)
+        // get the number of copy still using these data
+        // delete source if it's alone
+        if (nlink === 1) {
+          await fs.unlink(dedupPath)
+        } 
+      }catch(error){
+        // no problem if another process deleted the source or if we unlink directly the source file
+        if(error.code !== 'ENOENT'){
+          throw error
+        }
       }
-    } 
+    }
   }
-  
+
   // @todo : use a multiplatform package instead
-  async #getExtendedAttribute(file, attribueName) { 
-    const value = await execa('getfattr', ['-n',attribueName, '--only-value', this.getFilePath(file)])
-    return value
+  async #getExtendedAttribute(file, attribueName) {
+    const {stdout} = await execa('getfattr', ['-n', attribueName, '--only-value', this.getFilePath(file)])
+    return stdout
   }
   async #setExtendedAttribute(file, attribueName, value) {
-    await execa('setfattr', ['-n',attribueName,'-v',value,  this.getFilePath(file)])
+    await execa('setfattr', ['-n', attribueName, '-v', value, this.getFilePath(file)])
   }
 
   // create a hard link between to files
@@ -73,7 +80,7 @@ export default class DedupedLocalHandler extends LocalHandler {
 
   // split path to keep a sane number of file per directory
   #computeDeduplicationPath(hash) {
-    assert.strictEqual(hash.length % 4 , 0)
+    assert.strictEqual(hash.length % 4, 0)
     let path = this.#dedupFolder
     for (let i = 0; i < hash.length; i++) {
       if (i % 4 === 0) {
@@ -81,21 +88,22 @@ export default class DedupedLocalHandler extends LocalHandler {
       }
       path += hash[i]
     }
-    path += hash + '.source'
+    path += '.source'
     return path
   }
 
-
-  async #writeDeduplicationSource( hash, data) {
-    const path = this.#computeDeduplicationPath(hash) 
+  async #writeDeduplicationSource(hash, data) {
+    const path = this.#computeDeduplicationPath(hash)
     try {
-      // flags ensures it fails if it already exists 
-      await this._outputFile(path,data, { flags: 'wx' })
+      // flags ensures it fails if it already exists
+      await this._outputFile(path, data, { flags: 'wx' })
     } catch (error) {
       // if it is alread present : not a problem
-      if (error.code !== 'EEXIST') {
-        throw error
+      if (error.code === 'EEXIST') {
+        // it should already have the extended attributes, nothing more to do
+        return 
       }
+      throw error
     }
 
     try {
